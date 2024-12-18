bc32415f4b81c01b8ba7fe111d79267efdfcd9dd
Wagner <wagnersanches5@hotmail.com>
2024-12-18 18:08:18 -0300

    Add error response class to provide more information

--- src/main/java/com/demo/URL/Shortener/controller/ShortnerExceptionController.java
+++ src/main/java/com/demo/URL/Shortener/controller/ShortnerExceptionController.java
@@ -1,17 +1,37 @@
 package com.demo.URL.Shortener.controller;
 
+import com.demo.URL.Shortener.dtos.ErrorResponseDTO;
 import com.demo.URL.Shortener.exceptions.URLNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class ShortnerExceptionController {
 
-    @ExceptionHandler
-    public ResponseEntity<String> exception(URLNotFoundException URLNotFoundException) {
-        return new ResponseEntity<>("ShortCode does not exist!", HttpStatus.NOT_FOUND);
+    @ExceptionHandler(URLNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> urlNotFoundException(URLNotFoundException URLNotFoundException) {
+        return new ResponseEntity<>(ErrorResponseDTO.builder()
+                .code(HttpStatus.NOT_FOUND.value())
+                .status("NOT FOUND")
+                .message("ShortCode does not exist!")
+                .timestamp(LocalDateTime.now())
+                .build(), HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> exception(Exception exception) {
+        return new ResponseEntity<>(ErrorResponseDTO.builder()
+                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .status("INTERNAL SERVER ERROR")
+                .timestamp(LocalDateTime.now())
+                .stacktrace(exception.getMessage())
+                .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
 
 }
---
+++ src/main/java/com/demo/URL/Shortener/dtos/ErrorResponseDTO.java
@@ -0,0 +1,21 @@
+package com.demo.URL.Shortener.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class ErrorResponseDTO {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+
+    private int code;
+    private String status;
+    private String message;
+    private String stacktrace;
+
+}
