b7182b7c319564a941b873a9659f708650c2f75f
Wagner <wagnersanches5@hotmail.com>
2024-12-19 18:14:05 -0300

    Tested save, delete, and update methods in ShortnerRepository

--- src/test/java/com/demo/URL/Shortener/repositoy/ShortnerRepositoryTest.java
+++ src/test/java/com/demo/URL/Shortener/repositoy/ShortnerRepositoryTest.java
@@ -4,31 +4,33 @@ import com.demo.URL.Shortener.entities.ShortnerUrlEntity;
 import com.demo.URL.Shortener.repository.ShortnerRepository;
 import com.demo.URL.Shortener.utils.ShortCodeGeneratorUtil;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ShortnerRepositoryTest {
 
     @Autowired
     private ShortnerRepository repository;
 
     private ShortnerUrlEntity shortnerUrlEntity;
+    private final String URL = "localhost:8080/teste";
 
     @BeforeAll
     public void setup() {
-        shortnerUrlEntity = ShortnerUrlEntity.builder()
-                .url("localhost:8080/teste")
+        this.shortnerUrlEntity = ShortnerUrlEntity.builder()
+                .url(URL)
                 .shortCode(ShortCodeGeneratorUtil.generate())
                 .createdAt(LocalDateTime.now())
                 .updatedAt(LocalDateTime.now())
@@ -37,20 +39,90 @@ public class ShortnerRepositoryTest {
     }
 
     @Test
-    @Order(2)
+    @Order(1)
     @Rollback(value = false)
     public void saveUrl() {
-        repository.save(shortnerUrlEntity);
+        System.out.println("------------------- saveUrl() ------------------");
 
-        System.out.println("------------------" + shortnerUrlEntity);
-        Assertions.assertThat(shortnerUrlEntity.getShortnerUrlId()).isGreaterThan(0);
+        repository.save(this.shortnerUrlEntity);
+
+        System.out.println(this.shortnerUrlEntity);
+        Assertions.assertThat(this.shortnerUrlEntity.getShortnerUrlId()).isGreaterThan(0);
     }
 
     @Test
-    @Order(1)
+    @Order(2)
+    public void getAllUrls() {
+        System.out.println("------------------- getAllUrls() ------------------");
+        System.out.println(this.shortnerUrlEntity);
+        List<ShortnerUrlEntity> urls = repository.findAll();
+
+        Assertions.assertThat(urls)
+                .isNotEmpty()
+                .anyMatch(url ->
+                        url.getShortCode().equals(this.shortnerUrlEntity.getShortCode()));
+    }
+
+    @Test
+    @Order(3)
+    public void getUrl() {
+        System.out.println("------------------- getUrl() ------------------");
+        Optional<ShortnerUrlEntity> url = Optional.ofNullable(
+                repository.findByShortCode(this.shortnerUrlEntity.getShortCode())
+        );
+
+        Assertions.assertThat(url).isNotEmpty();
+    }
+
+    @Test
+    @Order(4)
+    public void updateUrl() {
+        System.out.println("------------------- updateUrl() ------------------");
+
+        Optional<ShortnerUrlEntity> url = Optional.ofNullable(
+                repository.findByShortCode(this.shortnerUrlEntity.getShortCode())
+        );
+
+        Assertions.assertThat(url)
+                .isPresent()
+                .get()
+                .extracting(ShortnerUrlEntity::getUrl)
+                .isEqualTo(URL);
+
+        String shortCode = this.shortnerUrlEntity.getShortCode();
+        String newUrl = "localhost:3001/updated";
+        this.shortnerUrlEntity.updateUrl(newUrl);
+        repository.save(this.shortnerUrlEntity);
+
+        url = Optional.ofNullable(
+                repository.findByShortCode(this.shortnerUrlEntity.getShortCode())
+        );
+
+        Assertions.assertThat(url)
+                .isPresent()
+                .get()
+                .extracting(ShortnerUrlEntity::getUrl)
+                .isEqualTo(newUrl);
+
+        Assertions.assertThat(url)
+                .isPresent()
+                .get()
+                .extracting(ShortnerUrlEntity::getShortCode)
+                .isEqualTo(shortCode);
+    }
+
+    @Test
+    @Order(5)
+    @Rollback(value = false)
     public void deleteUrl() {
-        System.out.println("------------------" + repository.findByShortCode(shortnerUrlEntity.getShortCode()));
+        System.out.println("------------------- deleteUrl() ------------------");
+        System.out.println(this.shortnerUrlEntity);
         repository.delete(shortnerUrlEntity);
-        System.out.println("------------------" + repository.findByShortCode(shortnerUrlEntity.getShortCode()));
+
+        Optional<ShortnerUrlEntity> deletedShortnerUrlEntity = Optional.ofNullable(
+                repository.findByShortCode(this.shortnerUrlEntity.getShortCode())
+        );
+        Assertions.assertThat(deletedShortnerUrlEntity).isEmpty();
+
     }
 }
